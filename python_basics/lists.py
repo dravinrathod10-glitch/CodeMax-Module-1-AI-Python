177f2dd96e46a85b9b4075c3e7c6b964cffe6cf7
dravinrathod10-glitch <dravinrathod10@gmail.com>
2026-09-02 20:30:06 +0530

    Add Python lists program

---
+++ python_basics/lists.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Fruits:", fruits)
+
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+
+fruits.append("Grapes")
+print("After adding a fruit:", fruits)
