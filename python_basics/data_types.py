6888cabd52a8b75984ebd29b85e5e6e4f01430fe
dravinrathod10-glitch <dravinrathod10@gmail.com>
2026-09-02 20:23:58 +0530

    Add Python data types program

---
+++ python_basics/data_types.py
@@ -0,0 +1,16 @@
+name = "Dravin"
+age = 20
+marks = 85.5
+is_student = True
+
+print("Name:", name)
+print("Type:", type(name))
+
+print("Age:", age)
+print("Type:", type(age))
+
+print("Marks:", marks)
+print("Type:", type(marks))
+
+print("Student:", is_student)
+print("Type:", type(is_student))
